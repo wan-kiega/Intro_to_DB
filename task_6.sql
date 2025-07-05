6b0f37556cd07896e16cf0373a4c1723508bebb1
Joy Kiega <jwkiega@gmail.com>
2025-07-05 15:56:56 +0300

    changes made

--- task_6.sql
+++ task_6.sql
@@ -1,11 +1,4 @@
--- task_6.sql: Insert multiple rows into the customers table
-
--- Use the database passed as argument
-USE alx_book_store;
-
--- Insert multiple customer records
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
-VALUES 
+INSERT INTO customer (customer_id, customer_name, email, address)VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
