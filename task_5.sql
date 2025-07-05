095c84b3bc7c4d6a45048263075bc88e18f482d6
Joy Kiega <jwkiega@gmail.com>
2025-07-05 16:00:45 +0300

    task completed

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,2 @@
--- task_5.sql: Insert one customer into customers table
-
-USE alx_book_store;
-
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
-VALUES (2, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
